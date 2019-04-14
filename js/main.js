922c26d719194d78fb7fc57fc0007b13e5f57499
jrstar08 <39608912+jrstar08@users.noreply.github.com>
2019-04-14 23:41:00 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//javascript, jQuery
+var once = true;
+console.log('Output: once', once);
+function search() {
+	var search = $('#search_name').val();
+	var title;
+	// console.log(search);
+	var xhr = $.get(
+		'http://api.giphy.com/v1/gifs/search?q=sign%20with%20robert&api_key=N6fDSNgTp2KwnvoO8VNUz10tsryx1fTS&limit=30&offset=200'
+	);
+	// var xhr = $.get("http://api.giphy.com/v1/gifs/l4q88nfCo5ipr79Ly?api_key=N6fDSNgTp2KwnvoO8VNUz10tsryx1fTS");
+
+	xhr.done(function(data) {
+		// console.log("available:");
+		for (i in data.data) {
+			title = data.data[i].title;
+			title = title.replace(' GIF by Sign with Robert', '');
+			title = title.replace('sign language', '');
+			title = title.replace('asl', '');
+
+			if (title != '' && title != ' ' && once) console.log(title);
+
+			title = data.data[i].title;
+			if (title.search(search) != -1)
+				// $('#result').append('<p>'+title+'</p>');
+
+				$('#result').append(
+					'<img src="' +
+						data.data[i].images.preview_gif.url +
+						'" style="width: 48vw;">'
+				);
+		}
+		once = false;
+	});
+}
